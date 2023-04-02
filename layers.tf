460d921a93052fd9045cc1112b0e571fc3f891f1
jlonge4 <josh.longenecker1@gmail.com>
2023-04-02 10:49:58 -0400

    requests-layer

--- layers.tf
+++ layers.tf
@@ -1,13 +1,13 @@
-resource "aws_lambda_layer_version" "excel-layer" {
-  filename   = "${path.module}/lambda_layers/ms-xl-layer.zip"
-  layer_name = "excel-layer"
+# resource "aws_lambda_layer_version" "excel-layer" {
+#   filename   = "${path.module}/lambda_layers/ms-xl-layer.zip"
+#   layer_name = "excel-layer"
 
-  compatible_runtimes = ["python3.9"]
-}
+#   compatible_runtimes = ["python3.9"]
+# }
 
-resource "aws_lambda_layer_version" "requests-layer" {
-  filename   = "${path.module}/lambda_layers/requests-layer.zip"
-  layer_name = "requests-layer"
+resource "aws_lambda_layer_version" "requests-layer-tf" {
+  filename   = "${path.module}/layers/requests.zip"
+  layer_name = "requests-layer-tf"
 
   compatible_runtimes = ["python3.9"]
 }
