daffd8607e98e074d536c178597e54cdfb4ef2a8
jlonge4 <josh.longenecker1@gmail.com>
2023-04-02 14:31:44 -0400

    add twilio/bs4

--- layers.tf
+++ layers.tf
@@ -1,13 +1,27 @@
-# resource "aws_lambda_layer_version" "excel-layer" {
-#   filename   = "${path.module}/lambda_layers/ms-xl-layer.zip"
-#   layer_name = "excel-layer"
+resource "aws_lambda_layer_version" "excel-layer-tf" {
+  filename   = "${path.module}/layers/xlsxwriter.zip"
+  layer_name = "excel-layer-tf"
 
-#   compatible_runtimes = ["python3.9"]
-# }
+  compatible_runtimes = ["python3.9", "python3.8"]
+}
 
 resource "aws_lambda_layer_version" "requests-layer-tf" {
   filename   = "${path.module}/layers/requests.zip"
   layer_name = "requests-layer-tf"
+
+  compatible_runtimes = ["python3.9", "python3.8"]
+}
+
+resource "aws_lambda_layer_version" "twilio-layer-tf" {
+  filename   = "${path.module}/layers/twilio.zip"
+  layer_name = "twilio-layer-tf"
+
+  compatible_runtimes = ["python3.9", "python3.8"]
+}
+
+resource "aws_lambda_layer_version" "bs4-layer-tf" {
+  filename   = "${path.module}/layers/bs4.zip"
+  layer_name = "beautsoup-layer-tf"
 
   compatible_runtimes = ["python3.9", "python3.8"]
 }
